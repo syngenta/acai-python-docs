96beef46c0b759b296c471b50747dcaba1dad388
Paul Cruse III <paulcruse3@gmail.com>
2023-10-18 17:07:25 -0500

    need more stuff

--- examples/s3/app/handler.py
+++ examples/s3/app/handler.py
@@ -1,7 +1,12 @@
 from acai_aws.s3.requirements import requirements
 
 
-@requirements(operations=['created', 'deleted'])
+@requirements(
+    operations=['created', 'deleted'],
+    get_object=True,
+    data_type='json'
+)
 def handle(event):
     for record in event.records:
         print(record)
+        print(record.body)
